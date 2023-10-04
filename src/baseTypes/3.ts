c79c082df70114adfa15ee01da6392416a6035ba
Irulik <123405063+Irulik@users.noreply.github.com>
2023-10-04 21:50:47 +0200

    Update 3.ts

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -3,4 +3,10 @@ some = 'Text';
 let str: string;
 str = some;
 
+if (typeof some === 'string') {
+    str = some; // Перевіряємо тип перед привласненням
+} else {
+    str = 'Default Value';
+}
+
 export {};
