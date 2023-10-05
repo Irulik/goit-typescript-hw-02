292e6ce4e5f593653107bf02f458d340233c142c
Irulik <123405063+Irulik@users.noreply.github.com>
2023-10-05 15:10:48 +0200

    Update 5.ts

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,20 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
+
+// Приклади використання інтерфейсу KeyValuePair
+const numberPair: KeyValuePair<number, string> = {
+  key: 1,
+  value: 'One',
+};
+
+const stringPair: KeyValuePair<string, boolean> = {
+  key: 'isTrue',
+  value: true,
+};
 
 export {};
