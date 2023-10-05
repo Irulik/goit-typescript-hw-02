57a9461797291ce0f61ca6472070b2c9eed0468a
Irulik <123405063+Irulik@users.noreply.github.com>
2023-10-05 15:01:21 +0200

    Update 2.ts

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,7 +11,7 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+function compare<T extends AllType, U extends AllType>(top: T, bottom: U): AllType {
   return {
     name: top.name,
     color: top.color,
@@ -19,5 +19,23 @@ function compare (top, bottom): AllType {
     weight: bottom.weight,
   }
 }
+
+// Приклад використання функції compare:
+const obj1 = {
+  name: 'Object 1',
+  position: 1,
+  color: 'red',
+  weight: 10,
+};
+
+const obj2 = {
+  name: 'Object 2',
+  position: 2,
+  color: 'blue',
+  weight: 20,
+};
+
+const result = compare(obj1, obj2);
+console.log(result);
 
 export {};
